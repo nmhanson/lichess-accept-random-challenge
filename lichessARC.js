f90abbdcca38b292643dbda08d23ec541afb895d
Thijs van den Hout <thijsvandenhout@live.nl>
2020-06-15 13:44:17 +0200

    new subscriber backend

--- lichessARC.js
+++ lichessARC.js
@@ -1,97 +1,99 @@
-function setNameColors(){
-  fetch('https://tvdhout.nl/getsubs.php', {method: "GET"})
-  .then((resp) => resp.text())
-  .then(function(data){
-    subs = data.split("<br>");
-    return subs;
-  }).then(function(subs){
-    var challenges = document.getElementsByClassName("challenge");
+const URL = 'https://tvdhout.com/ericrosen/get/'
 
-    for (var i = 0; i < challenges.length; i++) {
-      challengerName = challenges[i].getElementsByClassName("user-link")[0].innerText.split(" ")[0].toLowerCase();
-      if(subs.includes(challengerName)){
-        challenges[i].getElementsByClassName("user-link")[0].style.color = "#9e84ce";
-      }
-      else{
-        challenges[i].getElementsByClassName("user-link")[0].style.color = "#ADADAF";
-      }
-    }
-  })
-  .catch(function(error){
-    alert(error);
-  });
+function getSubs(){
+	return fetch(URL, {method: "GET"})
+	.then((resp) => resp.text())
+	.then(function(data){
+		data = data.toLowerCase();
+		subs = data.split(" ");
+		return subs;
+	});
+}
+
+function setNameColors(){
+	getSubs()
+	.then(function(subs){
+		var challenges = document.getElementsByClassName("challenge");
+
+		for (var i = 0; i < challenges.length; i++) {
+			challengerName = challenges[i].getElementsByClassName("user-link")[0].innerText.split(" ")[0].toLowerCase();
+			if(subs.includes(challengerName)){
+				challenges[i].getElementsByClassName("user-link")[0].style.color = "#9e84ce";
+			}
+		}
+	})
+	.catch(function(error){
+		console.log(error);
+	});
 }
 
 function acceptChallenge() {
-  var challenges = document.getElementsByClassName("challenge");
-  var challenge = challenges[Math.floor(Math.random() * challenges.length)]
-  challenge.getElementsByClassName("accept")[0].click();
+	var challenges = document.getElementsByClassName("challenge");
+	var challenge = challenges[Math.floor(Math.random() * challenges.length)]
+	challenge.getElementsByClassName("accept")[0].click();
 }
 
 function acceptSubChallenge() {
-  /**
-  Function and service by Stockvis.
-  Queries the database of IMRosen subs with their lichess handles and filters challenges .
-  **/
-  var challenges = document.getElementsByClassName("challenge");
+	/**
+	Function and service by Stockvis.
+	Queries the database of IMRosen subs with their lichess handles and filters challenges .
+	**/
+	var challenges = document.getElementsByClassName("challenge");
 
-  fetch('https://tvdhout.nl/getsubs.php', {method: "GET"})
-  .then((resp) => resp.text())
-  .then(function(data){
-    var subs = data.split("<br>");
-    return subs;
-  })
-  .then(function(subs){
-  	
-    var subChallenges = [];
+	getSubs()
+	.then(function(subs){
+		
+		var subChallenges = [];
 
-    for (var i = 0; i < challenges.length; i++) {
-      challengerName = challenges[i].getElementsByClassName("user-link")[0].innerText.split(" ")[0].toLowerCase();
-      if(subs.includes(challengerName)){
-        subChallenges.push(challenges[i]);
-      }
-    }
+		for (var i = 0; i < challenges.length; i++) {
+			challengerName = challenges[i].getElementsByClassName("user-link")[0].innerText.split(" ")[0].toLowerCase();
+			if(subs.includes(challengerName)){
+				subChallenges.push(challenges[i]);
+			}
+		}
 
-    if(subChallenges.length > 0){
-      var challenge = subChallenges[Math.floor(Math.random() * subChallenges.length)];
-      challenge.getElementsByClassName("accept")[0].click();
-    }
+		if(subChallenges.length > 0){
+			var challenge = subChallenges[Math.floor(Math.random() * subChallenges.length)];
+			challenge.getElementsByClassName("accept")[0].click();
+		}
 
-  })
-  .catch(function(error){
-    alert(error);
-  })
+	})
+	.catch(function(error){
+		console.log(error);
+	})
 }
+
 
 function init(container) {
-  var div = document.createElement("div");
-  div.innerText = "Accept random challenge";
-  div.id = "lichess-arc";
-  div.onclick = acceptChallenge;
-  container.prepend(div);
+	var div = document.createElement("div");
+	div.innerText = "Accept random challenge";
+	div.id = "lichess-arc";
+	div.onclick = acceptChallenge;
+	container.prepend(div);
 
-  var subdiv = document.createElement("div");
-  subdiv.innerText = "Accept subscriber challenge";
-  subdiv.id = "lichess-arc-sub";
-  subdiv.onclick = acceptSubChallenge;
-  container.prepend(subdiv);
-  setNameColors();
+	var subdiv = document.createElement("div");
+	subdiv.innerText = "Accept subscriber challenge";
+	subdiv.id = "lichess-arc-sub";
+	subdiv.onclick = acceptSubChallenge;
+	container.prepend(subdiv);
 }
+
 function loadContainer() {
-  e = document.getElementById('challenge-toggle');
-  e.click(); // open challanges menu
-  e.click(); // close challanges menu
+	e = document.getElementById('challenge-toggle');
+	e.click(); // open challanges menu
+	e.click(); // close challanges menu
 }
+
 function initWhenContainerLoaded() {
-  var container = document.getElementById("challenge-app");
-  if (container.className.indexOf("rendered") > 0) {
-    init(container);
-  } else {
-    loadContainer();
-    setTimeout(initWhenContainerLoaded, 100);
-  }
-  document.getElementById("challenge-app").addEventListener("mouseenter", setNameColors);
-  document.getElementById('challenge-toggle').addEventListener("click", setNameColors);
+	var container = document.getElementById("challenge-app");
+	if (container.className.indexOf("rendered") > 0) {
+		init(container);
+	} else {
+		// loadContainer();
+		setTimeout(initWhenContainerLoaded, 100);
+	}
+	document.getElementById('challenge-toggle').addEventListener("click", setNameColors);
+	document.getElementById("challenge-app").addEventListener("mouseenter", setNameColors);
 }
 
 initWhenContainerLoaded();
